42844884896c2506b4242cf8d3e5cea87cc1a07b
Jesse Strode <jesse_strode@condenast.com>
2020-05-03 14:08:11 -0400

    Updates

---
+++ precache-manifest.9fd94cccfdc894c7114109e3516e8128.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f3798a5289343e337aa9f602540106",
+    "url": "/trait-table-rework/index.html"
+  },
+  {
+    "revision": "369c21244e8fcd247630",
+    "url": "/trait-table-rework/static/css/main.55826da6.chunk.css"
+  },
+  {
+    "revision": "48e2732e34f7bdf99201",
+    "url": "/trait-table-rework/static/js/2.f972f432.chunk.js"
+  },
+  {
+    "revision": "260254c2b55f2376c2a0340f77dec2ca",
+    "url": "/trait-table-rework/static/js/2.f972f432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "369c21244e8fcd247630",
+    "url": "/trait-table-rework/static/js/main.58ebec61.chunk.js"
+  },
+  {
+    "revision": "62919fce4ebd617288f5",
+    "url": "/trait-table-rework/static/js/runtime-main.470d81dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trait-table-rework/static/media/logo.5d5d9eef.svg"
+  }
+]);
